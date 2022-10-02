0c54d6287e3b73524609e1a3e176e94e39f69f4c
enrval <enriquefvalentino@gmail.com>
2022-10-01 22:32:53 -0400

    added test file for US07 and US08

---
+++ project4/ev_tests.py
@@ -0,0 +1,38 @@
+import unittest
+
+class Test(unittest.TestCase):
+
+    #TODO Create Test Cases
+
+    def test_US07_one(self):
+        self.assertEqual("1","1")
+
+    def test_US07_two(self):
+        self.assertEqual("1","1")
+
+    def test_US07_three(self):
+        self.assertEqual("1","1")
+
+    def test_US07_four(self):
+        self.assertEqual("1","1")
+
+    def test_US07_five(self):
+        self.assertEqual("1","1")    
+
+    def test_US08_one(self):
+        self.assertEqual("1","1")
+
+    def test_US08_two(self):
+        self.assertEqual("1","1")
+
+    def test_US08_three(self):
+        self.assertEqual("1","1")
+
+    def test_US08_four(self):
+        self.assertEqual("1","1")
+
+    def test_US08_five(self):
+        self.assertEqual("1","1")    
+
+if __name__ == '__main__':
+    unittest.main()
