3676f358c2d5f04b220e3d329d5894dcae2b3ab0
enrval <enriquefvalentino@gmail.com>
2022-10-03 16:03:51 -0400

    output test results to a file

--- project4/acceptance_test.py
+++ project4/acceptance_test.py
@@ -1,7 +1,9 @@
 from project4 import Family
+import sys
 
 if __name__ == '__main__':
     filename = '../data/testfamily.ged'
     family = Family(filename)
     family.create_family(filename)
-    print(family)
+    with open('testoutput.txt', 'w') as sys.stdout:
+        print(family)
