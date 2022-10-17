8308ea62b8ef5bb7c0e984370b251eb904533bc5
Andrew Narvaez <anarvaez12@verizon.net>
2022-10-17 10:57:28 -0400

    Added test cases for US09 and US10

--- project6/an_tests.py
+++ project6/an_tests.py
@@ -5,11 +5,26 @@ from main import Family
 
 class Test(unittest.TestCase):
 
+    def test_US09_US10_one(self):
+        filename = "../data/US09-10/US09-10_good.ged"
+        fam = Family(filename)
+        fam.create_family(filename)
+        
+        self.assertTrue(fam.exceptions == [])
+
     def test_US09_one(self):
-        pass
+        filename = "../data/US09/US09_bad.ged"
+        fam = Family(filename)
+        fam.create_family(filename)
+        print(fam)
+        self.assertTrue(fam.exceptions != [])
 
     def test_US10_one(self):
-        pass
+        filename = "../data/US10/US10_bad.ged"
+        fam = Family(filename)
+        fam.create_family(filename)
+        print(fam)
+        self.assertTrue(fam.exceptions != [])
 
 if __name__ == '__main__':
     unittest.main()
