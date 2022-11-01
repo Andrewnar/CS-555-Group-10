ff49b77aa13f17d018143107ff42fc1fa95d8d6a
Ryan Chin <ryanchin26@gmail.com>
2022-10-31 21:49:37 -0400

    bad

--- project8/ch_tests.py
+++ project8/ch_tests.py
@@ -5,19 +5,19 @@ from main import Family
 
 class Test(unittest.TestCase):
 
-    # def test_US19_US20_one(self):
-    #     filename = "../data/US19/US19_good.ged"
-    #     fam = Family(filename)
-    #     fam.create_family(filename)
+    def test_US19_US20_one(self):
+        filename = "../data/US19/US19_good.ged"
+        fam = Family(filename)
+        fam.create_family(filename)
         
-    #     self.assertTrue(fam.exceptions == [])
+        self.assertTrue(fam.exceptions == [])
 
-    # def test_US19_one(self):
-    #     filename = "../data/US19/US19_bad.ged"
-    #     fam = Family(filename)
-    #     fam.create_family(filename)
-    #     # print(fam)
-    #     self.assertTrue(fam.exceptions != [])
+    def test_US19_one(self):
+        filename = "../data/US19/US19_bad.ged"
+        fam = Family(filename)
+        fam.create_family(filename)
+        # print(fam)
+        self.assertTrue(fam.exceptions != [])
 
     # aunt and uncle are married
     def test_US20_one(self):
